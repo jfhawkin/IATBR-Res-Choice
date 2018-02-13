faede6e39289068cad2f824d3e889df90032eb3c
Jason Hawkins <jfhawkin@gmail.com>
2018-02-13 17:18:43 -0500

    step 1 semi-working
    Initial bid choice model estimated and giving results for 4 iterations.
    Need to code score.

--- GTA_IATBR.py
+++ GTA_IATBR.py
@@ -12,8 +12,9 @@ import pandas as pd
 #import shapefile #the pyshp module
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 import patsy
-from bid_choice import BCLogit as bc
+import bid_choice as bc
 
 
 
@@ -37,7 +38,7 @@ ASC1 - fixed at 0
 1. ASC3 - income constant for quintile 3
 2. ASC4 - income constant for quintile 4
 3. ASC5 - income constant for quintile 5
-4. house - indicates the unit is a house x HH has more than 2 members
+4. house - indicates the zone structure type is house x HH has more than 2 members
 5. high_inc - indicates the househould is high income (quintile 4/5) and zone has high average income
 6 low_inc  - indicates the household is high income (quintile 4/5) and zone has low average income
 7. access - Accessibility measure from mode choice
@@ -46,67 +47,103 @@ ASC1 - fixed at 0
 10. gov - area of land devoted to government
 11. park - area of land devoted to parks
 12. res - area of land devoted to residential
-13. num_job - number of jobs in zone
+13. num_job - number of jobs in zone in x100 jobs
 14. num_school - number of schools in zone
-15. surface - surface area of dwelling m2 x log(HH size)
-16. price - distinguishes between if hh rents/owns and house/townhouse/apartment
+15. surface - surface area of dwelling in x100 m2 x log(HH size)
+16. obs price - distinguishes between if hh rents/owns and house/townhouse/apartment in $/100 m2 over 25 years (parameter is gamma in log likelihood)
 17. a - a price-specific constant
-18. gamma - a price-specific constant to transform price utility into dollars
-19. sigma - a price-specific constant
+18. sigma - a price-specific constant
 """
 
-data = pd.read_csv('../data/testData.csv',',')
+dfTAZ = pd.read_csv('../data/testDataTAZ.csv',',')
+dfHH = pd.read_csv('../data/testDataHH.csv',',')
+HH_COLS = dfHH.shape[1]
+TAZ_COLS = dfTAZ.shape[1]
+EXOG_COLS = 18 #CHANGE ME FOR EACH MODEL SPECIFICATION
+ALTS = len(dfTAZ.groupby(['struc_type','taz_region'])) #CHANGE ME FOR EACH MODEL SPECIFICATION
 
-#Income quintile thresholds in 2011 for Canada
+hh_taz_alts = np.zeros((len(dfHH)*ALTS,(HH_COLS+TAZ_COLS)))
+
+for i, row in dfHH.iterrows():
+    if i==0:
+        hh_taz_alts[i:(i+ALTS),0:HH_COLS] = np.repeat(row.as_matrix()[np.newaxis,:],ALTS,axis=0)
+        hh_taz_alts[i,HH_COLS:] = dfTAZ[dfTAZ['taz_struc']==row['taz_struc']].as_matrix()
+        
+        #remove the group for the chosen alternative and create random alternatives from the other groups
+        tempdfTAZ = dfTAZ[(dfTAZ['struc_type']!=hh_taz_alts[i,HH_COLS+2]) | (dfTAZ['taz_region']!=hh_taz_alts[i,HH_COLS+3])]
+        hh_taz_alts[(i+1):(i+ALTS),HH_COLS:] = tempdfTAZ.groupby(['struc_type','taz_region']).apply(lambda x: x.sample(1)).reset_index(drop=True).as_matrix()
+    else:
+        hh_taz_alts[i*ALTS:i*ALTS+ALTS,0:HH_COLS] = np.repeat(row.as_matrix()[np.newaxis,:],ALTS,axis=0)
+        hh_taz_alts[i*ALTS,HH_COLS:] = dfTAZ[dfTAZ['taz_struc']==row['taz_struc']].as_matrix()
+        
+        #remove the group for the chosen alternative and create random alternatives from the other groups
+        tempdfTAZ = dfTAZ[(dfTAZ['struc_type']!=hh_taz_alts[i*ALTS,HH_COLS+2]) | (dfTAZ['taz_region']!=hh_taz_alts[i*ALTS,HH_COLS+3])]
+        hh_taz_alts[i*ALTS+1:i*ALTS+ALTS,HH_COLS:] = tempdfTAZ.groupby(['struc_type','taz_region']).apply(lambda x: x.sample(1)).reset_index(drop=True).as_matrix()
+
+##Income quintile thresholds in 2014 for Canada
 incomeQuins = [28900,51700,101100,129400]
 
-endog = data['taz']
-exog = pd.DataFrame()
+endog_chosen = dfHH['taz_struc'].as_matrix()
+endog = hh_taz_alts[:,HH_COLS].tolist()
+#Repeat the endog_chosen array for each alternative
+endog_chosen = np.repeat(endog_chosen,ALTS)
+#Get the number of columns specific to each zone for each alternative
+exog = np.zeros((len(endog),EXOG_COLS))
 
-exog['ASC2'] = ((data['hh_income']<incomeQuins[1]) & (data['hh_income']>=incomeQuins[0])).astype(int)
-exog['ASC3'] = ((data['hh_income']<incomeQuins[2]) & (data['hh_income']>=incomeQuins[1])).astype(int)
-exog['ASC4'] = ((data['hh_income']<incomeQuins[3]) & (data['hh_income']>=incomeQuins[2])).astype(int)
-exog['ASC5'] = (data['hh_income']>incomeQuins[3]).astype(int)
-exog['house'] = data['house']*(data['hh_size']>2).astype(int)
-exog['high_inc'] = ((data['hh_income']>incomeQuins[2]) & (data['taz_income']>incomeQuins[2])).astype(int)
-exog['low_inc'] = ((data['hh_income']>incomeQuins[2]) & (data['taz_income']<incomeQuins[1])).astype(int)
-exog['ind'] = data['zone_ind']
-exog['com'] = data['zone_com']
-exog['gov'] = data['zone_gov']
-exog['park'] = data['zone_park']
-exog['res'] = data['zone_res']
-exog['num_jobs'] = data['taz_job']
-exog['num_schools'] = data['taz_school']
-exog['a'] = np.ones(endog.shape[0])
-exog['gamma'] = np.ones(endog.shape[0])
-exog['sigma'] = np.ones(endog.shape[0])
+#define a vector of chosen/unchosen
+chosen = (endog==endog_chosen).astype(int)
 
-tempExog = np.zeros((endog.shape[0],2))
-#Loop through rows of data and create an array with data as we go along. 
-#More efficient than series slices.
-for i, row in data.iterrows():
-    if row['house']*row['own']==1:
-        tempExog[i][0] = row['areaH']*np.log(row['hh_size'])
-        tempExog[i][1] = row['taz_priceH']
-    if row['house']*row['rent']==1:
-        tempExog[i][0] = row['areaHR']*np.log(row['hh_size'])
-        tempExog[i][1]= row['taz_priceHR']
-    if row['town']*row['own']==1:
-        tempExog[i][0] = row['areaT']*np.log(row['hh_size'])
-        tempExog[i][1] = row['taz_priceT']
-    if row['town']*row['rent']==1:
-        tempExog[i][0] = row['areaTR']*np.log(row['hh_size'])
-        tempExog[i][1] = row['taz_priceTR']
-    if row['condo']*row['own']==1:
-        tempExog[i][0] = row['areaC']*np.log(row['hh_size'])
-        tempExog[i][1] = row['taz_priceC']
-    if row['condo']*row['rent']==1:
-        tempExog[i][0] = row['areaCR']*np.log(row['hh_size'])
-        tempExog[i][1] = row['taz_priceCR']
+for i in range(hh_taz_alts.shape[0]):
+    #ASC2
+    exog[i,0] = ((hh_taz_alts[i,2]<incomeQuins[1]) & (hh_taz_alts[i,2]>=incomeQuins[0])).astype(int)
+    #ASC3
+    exog[i,1] = ((hh_taz_alts[i,2]<incomeQuins[2]) & (hh_taz_alts[i,2]>=incomeQuins[1])).astype(int)
+    #ASC4
+    exog[i,2] = ((hh_taz_alts[i,2]<incomeQuins[3]) & (hh_taz_alts[i,2]>=incomeQuins[2])).astype(int)
+    #ASC5
+    exog[i,3] = (hh_taz_alts[i,2]>incomeQuins[3]).astype(int)
+    #house
+    exog[i,4] = ((hh_taz_alts[i,HH_COLS+2]==1)*(hh_taz_alts[i,3]>2)).astype(int)
+    #high income
+    exog[i,5] = ((hh_taz_alts[i,2]>incomeQuins[2]) & (hh_taz_alts[i,HH_COLS+4]>incomeQuins[2])).astype(int)
+    #low income
+    exog[i,6] = ((hh_taz_alts[i,2]>incomeQuins[2]) & (hh_taz_alts[i,HH_COLS+4]>incomeQuins[2])).astype(int)
+    #industrial land
+    exog[i,7] = hh_taz_alts[i,HH_COLS+5]
+    #commercial land
+    exog[i,8] = hh_taz_alts[i,HH_COLS+6]
+    #government land
+    exog[i,9] = hh_taz_alts[i,HH_COLS+7]
+    #park land
+    exog[i,10] = hh_taz_alts[i,HH_COLS+8]
+    #residential land
+    exog[i,11] = hh_taz_alts[i,HH_COLS+9]
+    #number of jobs
+    exog[i,12] = hh_taz_alts[i,HH_COLS+10]/10**2
+    #number of schools
+    exog[i,13] = hh_taz_alts[i,HH_COLS+11]
+    #surface area of dwellings in TAZ
+    exog[i,14] = (np.log(hh_taz_alts[i,3])*hh_taz_alts[i,7]*hh_taz_alts[i,HH_COLS+12]+np.log(hh_taz_alts[i,3])*(1-hh_taz_alts[i,7])*hh_taz_alts[i,HH_COLS+13])/10**2
+    #price of dwellings in TAZ
+    exog[i,15] = (hh_taz_alts[i,7]*hh_taz_alts[i,HH_COLS+14]+(1-hh_taz_alts[i,7])*hh_taz_alts[i,HH_COLS+15])/10**2
 
-#Now add the two columns to the exog dataframe
-exog['surface'] = tempExog[:,0]
-exog['price'] = tempExog[:,1]
+#vector of ones for a and sigma
+exog[:,-2:] = np.ones((hh_taz_alts.shape[0],2))
 
-print(exog.head())
-print(exog.tail())
+params = np.array([-3.53,-3.71,-4.14,-1.41,-1.37,0.226,0.568,5.56,-4.67,0.1,0.1,0.1,0.1,0.1,0.002,0.446,1.04,-1.89])
+bc_mod = bc.BCLogit(endog,exog,EXOG_COLS,chosen,ALTS)
+loglike = bc_mod.loglike
+
+
+Nfeval = 1
+
+def callbackF(Xi):
+    global Nfeval
+    print('{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}'.format(Nfeval, Xi[0], Xi[1], Xi[2], loglike(Xi)))
+    Nfeval += 1
+    
+print('Beginning optimization...')
+print('{0:5s}   {1: 5s}   {2: 5s}   {3: 5s}   {4: 10s}'.format('Iter', 'X0', 'X1', 'X2', 'loglike'))
+
+mod = minimize(loglike, params, method='BFGS', tol=1e-3, jac=False, callback=callbackF, options={'maxiter':500, 'disp': True})
+print(mod.x)
--- bid_choice.py
+++ bid_choice.py
@@ -10,12 +10,9 @@ Latent price function of hedonic component and WTP component.
 
 """
 import numpy as np
-from statsmodels.base.data import handle_data
-from pandas.core.api import get_dummies
-from statsmodels.base.model import GenericLikelihoodModel
-import tools
+import pandas as pd
 
-class BCLogit(GenericLikelihoodModel):
+class BCLogit(object):
     __doc__ = """
     Generic logit-type model of bid-choice decision making according to Martinez paradigm
 
@@ -46,62 +43,22 @@ class BCLogit(GenericLikelihoodModel):
     K : float
         The actual number of parameters for the exogenous design.  Includes
         the constant if the design has one.
-    names : dict
-        A dictionary mapping the column number in `wendog` to the variables
-        in `endog`.
-    wendog : array
-        An n x j array where j is the number of unique categories in `endog`.
-        Each column of j is a dummy variable indicating the category of
-        each observation. See `names` for a dictionary mapping each column to
-        its category.
 
     Notes
     -----
     See developer notes for further information on `BCLogit` internals.
     """
 
-    def _handle_data(self, endog, exog, missing, hasconst, **kwargs):
-        if data_tools._is_using_ndarray_type(endog, None):
-            endog_dummies, ynames = _numpy_to_dummies(endog)
-            yname = 'y'
-        elif data_tools._is_using_pandas(endog, None):
-            endog_dummies, ynames, yname = tools.pd_to_dummies(endog)
-        else:
-            endog = np.asarray(endog)
-            endog_dummies, ynames = tools.np_to_dummies(endog)
-            yname = 'y'
+    def __init__(self,endog,exog,exog_var,chosen,alts,**kwds):
+        self.endog = endog
+        self.exog = exog
+        self.ch = chosen
+        self.J = alts
+        self.K = exog_var
 
-        if not isinstance(ynames, dict):
-            ynames = dict(zip(range(endog_dummies.shape[1]), ynames))
-
-        self._ynames_map = ynames
-        data = handle_data(endog_dummies, exog, missing, hasconst, **kwargs)
-        data.ynames = yname  # overwrite this to single endog name
-        data.orig_endog = endog
-        self.wendog = data.endog
-
-        # repeating from upstream...
-        for key in kwargs:
-            try:
-                setattr(self, key, data.__dict__.pop(key))
-            except KeyError:
-                pass
-        return data
-
-    def _init_(self,endog,exog,**kwds):
-        super(BCLogit, self).__init__(endog,exog,**kwds)
-        self.J = self.wendog.shape[1]
-        self.K = self.exog.shape[1]
-    
-    def pdf(self, eXB):
-        """
-        NotImplemented
-        """
-        raise NotImplementedError
-
-    def nloglikeobs(self,params):
-        ll = loglike(self.endog,self.exog,params)
-        return -ll
+#    def nloglikeobs(self,params):
+#        ll = loglike(self,params)
+#        return -ll
 
     def locFunc(self, X):
         """
@@ -122,9 +79,10 @@ class BCLogit(GenericLikelihoodModel):
         In the multinomial logit model.
         .. math:: \\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}
         """
-        eXB = np.column_stack((np.ones(len(X)), np.exp(X)))
+        eXB = np.exp(X)
+        eXB = eXB.reshape(-1,self.J)
         return eXB/eXB.sum(1)[:,None]
-
+    
     def pricefunc(self, X, pparams):
         """
         Multinomial logit cumulative distribution function.
@@ -144,17 +102,25 @@ class BCLogit(GenericLikelihoodModel):
         In the multinomial logit model.
         .. math:: \\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}
         
-        Don't forget you changed the GenericLikelihood model to include a function for nloglikeobs()
         """
-        eXB = np.column_stack((np.ones(len(X)), np.exp(X)))
-        ri = np.log(eXB.sum(0)[:,None])
-        Ri = np.column_stack((self.endog,self.exog[:,-3]))
-        Ri = np.unique(Ri,axis=0)[:-1,1]
-        pF = np.exp(-Ri-pparams[1]-pparams[0]*ri/(2*pparams[2]**2))
+        #Reference the utility values for rents to the endog list and take logsums over these indices
+        eXB = pd.DataFrame(data=np.exp(X),index=self.endog)
+        ri = np.log(eXB.groupby(eXB.index).sum())
+        #Reference the observed prices to the same set of endog and take the average between own/rent in this case
+        Ri = pd.DataFrame(data=self.exog[:,15],index=self.endog)
+        Ri = Ri.groupby(Ri.index).mean()
+        pF = np.exp((-Ri-pparams[1]-pparams[0]*ri)/(2*pparams[2]**2))
         pF = pF/np.sqrt(2*np.pi*pparams[2]**2)
-        if np.sum(pF)==0:
-            pF = np.ones((pF.shape[0],pF.shape[1]))
-        return np.column_stack((np.ones(pF.shape[0]),pF))
+        if pF.values.sum()==0:
+            pF = pd.DataFrame(data=np.ones((pF.shape[0],pF.shape[1])),index=self.endog)
+        
+        pF_hh = np.zeros(len(self.endog))
+        for i,v in enumerate(self.endog):
+            pF_hh[i] = pF.loc[pF.index==v].values
+            
+        pF_hh = pF_hh.reshape(-1,self.J)   
+        
+        return pF_hh
 
     def loglike(self, params):
         """
@@ -178,47 +144,11 @@ class BCLogit(GenericLikelihoodModel):
         where :math:`d_{ij}=1` if individual `i` chose alternative `j` and 0
         if not.
         """
-        params = params.reshape(self.K, -1, order='F')
-        d = self.wendog
+        d = self.ch.reshape(-1, self.J)
         locComp = self.locFunc(np.dot(self.exog[:,:-3],params[:-3]))
         priceComp = self.pricefunc(np.dot(self.exog[:,:-3],params[:-3]),params[-3:])
-        logprob = np.log(np.dot(locComp,priceComp))
-        return np.sum(d * logprob)
-
-
-    def loglikeobs(self, params):
-        """
-        NotImplemented
-        """
-        raise NotImplementedError
-    #    """
-    #    Log-likelihood of the multinomial logit model for each observation.
-
-    #    Parameters
-    #    ----------
-    #    params : array-like
-    #        The parameters of the multinomial logit model.
-
-    #    Returns
-    #    -------
-    #    loglike : ndarray (nobs,)
-    #        The log likelihood for each observation of the model evaluated
-    #        at `params`. See Notes
-
-    #    Notes
-    #    ------
-    #    .. math:: \\ln L_{i}=\\sum_{j=0}^{J}d_{ij}\\ln\\left(\\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}\\right)
-
-    #    for observations :math:`i=1,...,n`
-
-    #    where :math:`d_{ij}=1` if individual `i` chose alternative `j` and 0
-    #    if not.
-    #    """
-    #    params = params.reshape(self.K, -1, order='F')
-    #    d = self.wendog
-    #    logprob = np.log(self.cdf(np.dot(self.exog,params)))
-    #    return d * logprob
-
+        logProb = np.log(locComp*priceComp)
+        return -1*np.sum(d * logProb)
 
     def score(self, params):
         """
@@ -254,115 +184,3 @@ class BCLogit(GenericLikelihoodModel):
     #                                              params))[:,1:]
     #    #NOTE: might need to switch terms if params is reshaped
     #    return np.dot(firstterm.T, self.exog).flatten()
-
-
-    def loglike_and_score(self, params):
-        """
-        NotImplemented
-        """
-        raise NotImplementedError
-    #    """
-    #    Returns log likelihood and score, efficiently reusing calculations.
-
-    #    Note that both of these returned quantities will need to be negated
-    #    before being minimized by the maximum likelihood fitting machinery.
-
-    #    """
-    #    params = params.reshape(self.K, -1, order='F')
-    #    cdf_dot_exog_params = self.cdf(np.dot(self.exog, params))
-    #    loglike_value = np.sum(self.wendog * np.log(cdf_dot_exog_params))
-    #    firstterm = self.wendog[:, 1:] - cdf_dot_exog_params[:, 1:]
-    #    score_array = np.dot(firstterm.T, self.exog).flatten()
-    #    return loglike_value, score_array
-
-
-    def score_obs(self, params):
-        """
-        NotImplemented
-        """
-        raise NotImplementedError
-    #    """
-    #    Jacobian matrix for multinomial logit model log-likelihood
-
-    #    Parameters
-    #    ----------
-    #    params : array
-    #        The parameters of the multinomial logit model.
-
-    #    Returns
-    #    --------
-    #    jac : ndarray, (nobs, k_vars*(J-1))
-    #        The derivative of the loglikelihood for each observation evaluated
-    #        at `params` .
-
-    #    Notes
-    #    -----
-    #    .. math:: \\frac{\\partial\\ln L_{i}}{\\partial\\beta_{j}}=\\left(d_{ij}-\\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}\\right)x_{i}
-
-    #    for :math:`j=1,...,J`, for observations :math:`i=1,...,n`
-
-    #    In the multinomial model the score vector is K x (J-1) but is returned
-    #    as a flattened array. The Jacobian has the observations in rows and
-    #    the flatteded array of derivatives in columns.
-    #    """
-    #    params = params.reshape(self.K, -1, order='F')
-    #    firstterm = self.wendog[:,1:] - self.cdf(np.dot(self.exog,
-    #                                              params))[:,1:]
-    #    #NOTE: might need to switch terms if params is reshaped
-    #    return (firstterm[:,:,None] * self.exog[:,None,:]).reshape(self.exog.shape[0], -1)
-
-    def hessian(self, params):
-        """
-        NotImplemented
-        """
-        raise NotImplementedError
-    #    """
-    #    Multinomial logit Hessian matrix of the log-likelihood
-
-    #    Parameters
-    #    -----------
-    #    params : array-like
-    #        The parameters of the model
-
-    #    Returns
-    #    -------
-    #    hess : ndarray, (J*K, J*K)
-    #        The Hessian, second derivative of loglikelihood function with
-    #        respect to the flattened parameters, evaluated at `params`
-
-    #    Notes
-    #    -----
-    #    .. math:: \\frac{\\partial^{2}\\ln L}{\\partial\\beta_{j}\\partial\\beta_{l}}=-\\sum_{i=1}^{n}\\frac{\\exp\\left(\\beta_{j}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}\\left[\\boldsymbol{1}\\left(j=l\\right)-\\frac{\\exp\\left(\\beta_{l}^{\\prime}x_{i}\\right)}{\\sum_{k=0}^{J}\\exp\\left(\\beta_{k}^{\\prime}x_{i}\\right)}\\right]x_{i}x_{l}^{\\prime}
-
-    #    where
-    #    :math:`\\boldsymbol{1}\\left(j=l\\right)` equals 1 if `j` = `l` and 0
-    #    otherwise.
-
-    #    The actual Hessian matrix has J**2 * K x K elements. Our Hessian
-    #    is reshaped to be square (J*K, J*K) so that the solvers can use it.
-
-    #    This implementation does not take advantage of the symmetry of
-    #    the Hessian and could probably be refactored for speed.
-    #    """
-    #    params = params.reshape(self.K, -1, order='F')
-    #    X = self.exog
-    #    pr = self.cdf(np.dot(X,params))
-    #    partials = []
-    #    J = self.wendog.shape[1] - 1
-    #    K = self.exog.shape[1]
-    #    for i in range(J):
-    #        for j in range(J): # this loop assumes we drop the first col.
-    #            if i == j:
-    #                partials.append(\
-    #                    -np.dot(((pr[:,i+1]*(1-pr[:,j+1]))[:,None]*X).T,X))
-    #            else:
-    #                partials.append(-np.dot(((pr[:,i+1]*-pr[:,j+1])[:,None]*X).T,X))
-    #    H = np.array(partials)
-    #    # the developer's notes on multinomial should clear this math up
-    #    H = np.transpose(H.reshape(J,J,K,K), (0,2,1,3)).reshape(J*K,J*K)
-    #    return H
-
-    def fit(self, start_params = None, maxiter = 10000,maxfun = 5000,**kwds):
-        if start_params == None:
-            start_params = np.repeat(0,len(self.exog.columns)) 
-        return super(BCLogit, self).fit(start_params = start_params,maxiter = maxiter, maxfun = maxfun,**kwds)
